3159ed14e51da7bb2469324500b7057f3458952f
mateu52 <wuoelte@gmail.com>
2022-09-02 05:16:42 +0200

    fetch wymaga przyjcia danych 'users'

--- src/components/components/Users.js
+++ src/components/components/Users.js
@@ -2,14 +2,14 @@ import React, { Component }  from 'react';
 import { connect } from 'react-redux';
 import UsersList from './UsersList';
 import { fetchPosts } from '../redux';
-import { fetchPostReset } from '../redux';
+import { reset } from '../redux';
 
 class Users extends Component{
     fetchPosts = () => {
         this.props.fetchPosts();
     };
     fetchPostReset = () => {
-        this.props.fetchPostReset();
+        this.props.reset();
     };
     render(){
     const { posts, isLoading } = this.props;
@@ -37,6 +37,6 @@ const mapStateToProps = state => ({
 })
 const mapDispatchToProps = dispatch => ({
     fetchPosts: () => dispatch(fetchPosts()),
-    fetchPostReset: () => dispatch(fetchPostReset())
+    fetchPostReset: () => dispatch(reset())
 })
 export default connect(mapStateToProps, mapDispatchToProps)(Users);
--- src/components/redux.js
+++ src/components/redux.js
@@ -1,56 +1,69 @@
 
 
-const FETCH_POST_REQUESTED = 'FETCH_POST_REQUESTED'
-const FETCH_POST_SUCCEDED = 'FETCH_POST_SUCCEDED'
-const FETCH_POST_FAILED = 'FETCH_POST_FAILED'
-const FETCH_POST_RESET = 'FETCH_POST_RESET'
+const COUNTER_LOAD = 'COUNTER_LOAD'
+const COUNTER_RESET = 'COUNTER_RESET'
+const COUNTER_ADD = 'COUNTER_ADD'
+const LOAD_FILED = 'LOAD_FILED'
+const LOAD_SUCCEDED = 'LOAD_SUCCEDED'
 //actions
 const INITIAL_STATE = {
     posts: [],
     isLoading: false,
     isError: false
 }
-const fetchRequested = () => ({ type: FETCH_POST_REQUESTED});
-const fetchFailed = () => ({ type : FETCH_POST_FAILED});
-const fetchSucceded = data => ({ type: FETCH_POST_SUCCEDED, payload: data });
-export const fetchPostReset = () => ({ type: FETCH_POST_RESET });
+const loadSucceded = users =>({type: LOAD_SUCCEDED, payload: users})
+
+const add = user => ({type: COUNTER_ADD, user})
+
+ export const reset = () => ({type: COUNTER_RESET})
+
+const loadFailed = () =>({type: LOAD_FILED})
+
+const load = () =>({type: COUNTER_LOAD})
+
 export const fetchPosts = () => {
     return function(dispatch){
-        dispatch(fetchRequested())
+        dispatch(load())
         fetch("https://jsonplaceholder.typicode.com/posts")
         .then(response => response.json())
-        .then(data => dispatch(fetchSucceded(data.slice(0, 3))))
-        .catch(error => dispatch(fetchFailed()));
+        .then(data => dispatch(loadSucceded(data.slice(0, 3))))
+        .catch(error => dispatch(loadFailed()));
     }
 }
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case FETCH_POST_REQUESTED:
-            return {
-                ...state,
-                isLoading: true,
-                isError: false
-            }
-        case FETCH_POST_SUCCEDED:
+        case COUNTER_LOAD:
+                return {
+                    ...state,
+                    isLoading: true,
+                    isError: false
+                }
+        case LOAD_SUCCEDED:
             return {
                 ...state,
                 isLoading: false,
                 isError: false,
                 posts: action.payload
             }
-        case FETCH_POST_FAILED:
+        case COUNTER_RESET:
             return {
-                ...state,
+                posts: [],
                 isLoading: false,
                 isError: true
             }
-        case FETCH_POST_RESET:
+        case COUNTER_ADD:
+            return {
+                ...state,
+                isLoading: false,
+                isError: false,
+                posts: [ ...state.users, action.payload ]
+            }
+        case LOAD_FILED:
             return {
                 ...state,
                 isLoading: true,
-                isError: false,
-                posts: []
+                isError: false
             }
         default:
             return state;
